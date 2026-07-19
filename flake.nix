517eea3ac3b5b5481669282c8f64db0d195308db
septanto <s.s.adi.nugroho@gmail.com>
2026-07-19 22:33:11 +0700

    Init NixOS

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Septanto's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+	  home-manager.nixosModules.home-manager {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users."septanto" = import ./home.nix;
+	    home-manager.extraSpecialArgs = { inherit inputs; };
+	  }
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  home.username = "septanto";
+  home.homeDirectory = "/home/septanto";
+
+  # This value determines the Home Manager release compatible with
+  # your config. Do not change unless you know what you're doing.
+  home.stateVersion = "26.05";
+
+  # Let Home Manager manage itself
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "septanto";
+        email = "s.s.adi.nugroho@gmail.com";
+      };
+    };
+    signing = {
+      key = "/home/septanto/.ssh/id_ed25519.pub";
+      signByDefault = true;
+      format = "ssh";
+    };
+
+  };
+
+  home.packages = with pkgs; [
+    yazi
+  ];
+}
