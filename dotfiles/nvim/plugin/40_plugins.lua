39d1c714a1b8563f238f4ba0392701e6c2ca222d
septanto <s.s.adi.nugroho@gmail.com>
2026-07-24 22:23:36 +0700

    update kitty and nvim

--- dotfiles/nvim/plugin/40_plugins.lua
+++ dotfiles/nvim/plugin/40_plugins.lua
@@ -152,21 +152,6 @@ end)
 -- See `:h MiniSnippets.gen_loader.from_lang()`.
 later(function() add({ 'https://github.com/rafamadriz/friendly-snippets' }) end)
 
--- Honorable mentions =========================================================
-
--- 'mason-org/mason.nvim' (a.k.a. "Mason") is a great tool (package manager) for
--- installing external language servers, formatters, and linters. It provides
--- a unified interface for installing, updating, and deleting such programs.
---
--- The caveat is that these programs will be set up to be mostly used inside Neovim.
--- If you need them to work elsewhere, consider using other package managers.
---
--- You can use it like so:
--- now_if_args(function()
---   add({ 'https://github.com/mason-org/mason.nvim' })
---   require('mason').setup()
--- end)
-
 -- Render Markdown
 add({
   'https://github.com/MeanderingProgrammer/render-markdown.nvim',
